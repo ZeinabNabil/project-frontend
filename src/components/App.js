129dcb973bd3503cb7edac740618ef0a68621514
Hussein Salah <salahhussein176@gmail.com>
2022-08-05 19:24:48 +0200

    Hussein-BackednRegister

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,28 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "jquery/dist/jquery.min.js";
-import "bootstrap/dist/js/bootstrap.min.js";
-import MainAuthPage from "./Authentication/MainAuthPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./Authentication/Login";
-import Register from "./Authentication/Register";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../store';
+// CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+// Components
+import MainAuthPage from './Authentication/MainAuthPage';
+import Login from './Authentication/Login';
+import Register from './Authentication/Register';
 
 const App = () => {
   return (
-    // <div>
-    //   <MainAuthPage />
-    // </div>
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/auth" element={<MainAuthPage />}>
-          <Route exact path="login" element={<Login />} />
-          <Route exact path="register" element={<Register />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/auth" element={<MainAuthPage />}>
+            <Route exact path="login" element={<Login />} />
+            <Route exact path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
--- src/components/Authentication/FormComponents.js
+++ src/components/Authentication/FormComponents.js
@@ -1,7 +1,7 @@
-import React from "react";
-import style from "../../css/Authentication/Authentication.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import React from 'react';
+import style from '../../css/Authentication/Authentication.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classnames from 'classnames';
 const FormComponent = ({
   isHasIcon,
   icon,
@@ -11,11 +11,12 @@ const FormComponent = ({
   placeholder,
   onChange,
   value,
+  err,
 }) => {
   return (
     <div className="col-lg-12">
       <div className={`${style.input_div} input-group mb-3`}>
-        {isHasIcon === "yes" ? (
+        {isHasIcon === 'yes' ? (
           <div className={`${style.input_icon} input-group-prepend`}>
             <span className="input-group-text" id="basic-addon1">
               <FontAwesomeIcon icon={icon} className={style.highlight} />
@@ -25,12 +26,15 @@ const FormComponent = ({
         <input
           name={name}
           type={type}
-          className="form-control"
+          className={classnames('form-control', {
+            'is-invalid': err,
+          })}
           id={id}
           placeholder={placeholder}
           onChange={onChange}
           value={value}
         />
+        {err && <div className="invalid-feedback">{err}</div>}
       </div>
     </div>
   );
