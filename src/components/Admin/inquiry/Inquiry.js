4201c71c0449a0a117771578548d7f1ac4c63361
Hussein Salah <salahhussein176@gmail.com>
2022-08-17 00:27:15 +0200

    hussein-inq

--- src/components/Admin/inquiry/Inquiry.js
+++ src/components/Admin/inquiry/Inquiry.js
@@ -1,32 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import style from '../../../css/Admin/Inquiry/Inquiry.module.css';
 import InquiryAccordian from './InquiryAccordian';
-
-const Inquiry = () => {
+import { connect } from 'react-redux';
+import { showInquiries } from '../../../actions/inquiry.action';
+const Inquiry = (props) => {
+  useEffect(() => {
+    props.showInquiries();
+  }, []);
+  const { Inquiries, loading } = props.inquiry;
+  console.log(Inquiries);
+  var renderContent;
+  if (Inquiries === null || loading) {
+    renderContent = 'Loading';
+  } else {
+    renderContent = Inquiries.inquiries.map((inquiry, index) => {
+      return <InquiryAccordian data={inquiry} key={index} />;
+    });
+  }
   return (
     <div className={style.inquires}>
-      <div className="row">
-        <InquiryAccordian
-          eventKey="1"
-          data={{
-            name: 'Hussein',
-            email: 'asdas',
-            phone: 'asdsa',
-            body: 'asdsadddas',
-          }}
-        />
-        <InquiryAccordian
-          eventKey="1"
-          data={{
-            name: 'Hussein',
-            email: 'asdas',
-            phone: 'asdsa',
-            body: 'asdsadddas',
-          }}
-        />
-      </div>
+      <div className="row">{renderContent}</div>
     </div>
   );
 };
-
-export default Inquiry;
+const mapStateToProps = (state) => ({
+  inquiry: state.inquiry,
+});
+export default connect(mapStateToProps, { showInquiries })(Inquiry);
